79f9a7a77b57ac9842482c1b86e17939c5c9dad1
Scrom <formain337@gmail.com>
2026-07-06 12:44:05 +0530

    odd_even_linked_list code

---
+++ LinkedList/Singly/odd_even_linked_list/code.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode atEven = even;
+        ListNode temp = even.next;
+
+        int cnt = 3;
+
+        while(temp != null) {
+            if(cnt % 2 != 0) {
+                odd.next = temp;
+                odd = odd.next;
+            } else {
+                even.next = temp;
+                even = even.next;
+            }
+
+            temp = temp.next;
+            cnt++;
+        }
+
+        even.next = null;
+        odd.next = atEven;
+
+        return head;
+    }
+}
