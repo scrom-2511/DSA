fee0c1dd1b789da740692c6d12cd5fd91a0afb3f
Scrom <formain337@gmail.com>
2026-07-09 22:42:35 +0530

    add question link

--- Binary Search/onAnswers/Median_or_element_at_index_K_of_2_sorted_arr/code.java
+++ Binary Search/onAnswers/Median_or_element_at_index_K_of_2_sorted_arr/code.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
