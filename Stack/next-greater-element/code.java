e644b47ff0df135780ed80d446694260677098ee
Scrom <formain337@gmail.com>
2026-07-12 13:24:05 +0530

    next-greater-element code

---
+++ Stack/next-greater-element/code.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/next-greater-element-i/
+// https://takeuforward.org/data-structure/next-greater-element-using-stack/
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+
+            stack.push(nums2[i]);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
